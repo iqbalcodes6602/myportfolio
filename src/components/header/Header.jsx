53d1955583a8d112d74e3ef5e851e11afdf03b0a
noobcoder6602 <mohd.abd.6602@gmail.com>
2023-03-05 15:08:31 +0530

    react-tyed removed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import CTA from "./CTA";
 import ME from '../../assets/me.png'
 import HeaderSocials from "./HeaderSocials";
 import SplineChar from "./SplineChar";
-import Typed from "react-typed";
+// import Typed from "react-typed";
 
 const Header = () => {
   return (
@@ -18,7 +18,7 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1 style={{color: "var(--color-primary)"}}>Anas Iqbal</h1>
         <h5 className="text-light">
-          a <Typed
+          {/* a <Typed
             strings={[
               "Full Stack Developer",
               "Software Developer",
@@ -28,7 +28,7 @@ const Header = () => {
             typeSpeed={100}
             backSpeed={50}
             loop
-          />
+          /> */}
         </h5>
         {/* <CTA /> */}
         <HeaderSocials />
